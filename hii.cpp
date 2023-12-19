2e8d4bf5c9019d1e86e0c0ab88181b02861a370f
kingMahee <154326564+kingMahee@users.noreply.github.com>
2023-12-19 16:49:13 +0600

    Add files via upload

---
+++ hii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int i,j,a=1;
+    for(i=1;i<=4;i=i+1){
+        for(j=1;j<=a;j=j+1){
+            cout << "1";
+        }
+        a=a+1;
+        cout << endl;
+    }
+    
+}
